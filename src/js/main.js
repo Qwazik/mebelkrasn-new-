b1f9e87244d1067b770ff4ecb6e55c506f1d094a
Qwazik <qwazik21@gmail.com>
2017-07-06 18:04:24 +0300

    end

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
 $(function(){
+	new WOW().init();
+	$('.btn--transp').each(function(){
+		$(this).addClass('initialize');
+		$(this).wrapInner($('<span/>'));
+	});
 	$('.fancybox').fancybox();
 	$('input[type="tel"], input[name="phone"]').inputmask('+7(999)999-99-99');
-	$('.swiper-container').swiper({
+	$('#headerSlider').swiper({
 		pagination: $('#headerSwiperPagination')[0],
 		paginationClickable: true,
 		loop: true,
 		autoplay: 5000
 	});
+
+	$('#catalogSlider').swiper({
+		pagination: '.swiper-pagination',
+		loop: true,
+		autoplay: 10000,
+		paginationClickable: true,
+		spaceBetween: 40,
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+	});
+
 	$('.parallax-window').css('background-image','none');
 	$('.parallax-window').parallax({imageSrc:'/img/header-bg.jpg'});
 
@@ -16,11 +32,11 @@ $(function(){
 	$(window).on({
 		load:function(){
 			headerSliderWidth();	
-			// headerHeight();	
+			headerHeight();	
 		},
 		resize:function(){
 			headerSliderWidth();
-			// headerHeight();		
+			headerHeight();		
 		}
 	})
 });
@@ -30,9 +46,13 @@ function headerSliderWidth(){
 	$('.header-center .swiper-container').width(width);
 }
 
-// function headerHeight(){
-// 	var offset = 100;
-// 	while($(window).height() - offset > $('.main-header').outerHeight()){
-// 		$('.header-center .dt').height($('.header-center .dt').height()+1+'px');
-// 	}
-// }
+function headerHeight(){
+	var offset = 150;
+	var maxHeight = 530;
+	while($(window).height() - offset > $('.main-header').outerHeight()){
+		$('.header-center .dt').height($('.header-center .dt').height()+1+'px');
+		if($('.header-center .dt').outerHeight() >= maxHeight){
+			break;
+		}
+	}
+}
